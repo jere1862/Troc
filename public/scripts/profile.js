83206062696911f22fdab15fe1414fa135369610
Jean-Raphaël Poulin Arguin <arguinjr@gmail.com>
2018-01-20 18:44:00 -0500

    Profile route and page
    Gucci gang
    Gucci gang
    Gucci gang
    Gucci gang
    Gucci gang
    Gucci gang

---
+++ public/scripts/profile.js
@@ -0,0 +1,5 @@
+function show_box() {
+    console.log("killme")
+    document.getElementById('form_box').style.display = "block";
+    document.getElementById('offer_button').style.display = "none";
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var mockUserData = {
+    name: "Jean-Roger",
+    email: "rogerJJ@gmail.com",
+    phone: "418-225-1454",
+    address: "480 28e avenue nord, St-Georges",
+    offeredItems: ["Lord of the Rings: The twin towers", "Harry Potter: THE FIRST ONE"],
+    wantedItems: ["A song of ice and fire: tome one", "A drill"]
+};
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:userId', function (req, res, next) {
+    // res.render('profile', req.params);
+    console.log(mockUserData)
+    console.log(req.params)
+    res.render('profile', mockUserData)
 });
+
 
 module.exports = router;
