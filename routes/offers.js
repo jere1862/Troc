74081fa56e95fd70daa2762f0164d7df9d85e152
Jean-Raphaël Poulin Arguin <arguinjr@gmail.com>
2018-01-21 09:53:47 -0500

    add offer route

--- routes/offers.js
+++ routes/offers.js
@@ -16,6 +16,19 @@ router.get('/', function (req, res, next) {
     }
 });
 
+router.post('/create/:offerId', function (req, res, next) {
+    if (req.session.userId) {
+        database.insertOffer(req.body.from_user_id, req.body.to_user_id, req.body.from_user_offered_item_id, req.body.from_user_wanted_item_id, req.body.to_user_offered_item_id, req.body.to_user_wanted_item_id, '2017-02-02', '2017-02-04', function(err, result){
+            if (err) {
+                res.redirect('offers')
+            }
+            res.render('../index', {trades: result})
+        });
+    } else {
+        res.redirect('../index')
+    }
+});
+
 router.post('/accept/:offerId', function (req, res, next) {
     if (req.session.userId) {
         database.acceptOffer(req.params.offerId, function (err, result) {
